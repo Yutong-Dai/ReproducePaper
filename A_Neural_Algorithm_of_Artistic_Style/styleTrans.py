8ed43f7fc862ba6caad7ee3635b4f37b4b2ff098
Roth <rothdyt@gmail.com>
2021-03-21 23:17:21 -0400

    fix bugs

--- A_Neural_Algorithm_of_Artistic_Style/styleTrans.py
+++ A_Neural_Algorithm_of_Artistic_Style/styleTrans.py
@@ -2,7 +2,7 @@
 File: main.py
 Author: Yutong Dai (yutongdai95@gmail.com)
 File Created: 2021-03-21 22:19
-Last Modified: 2021-03-21 23:15
+Last Modified: 2021-03-21 23:17
 --------------------------------------------
 Description:
 '''
@@ -21,12 +21,12 @@ def style_transfer(c='./db/pikachu.jpg', s='./db/pikachu.jpg', epochs=400,
     model = model.cuda()
     # load image
     # contentImage
-    img = cv2.imread()
+    img = cv2.imread(c)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = cv2.resize(img, (1024, 768))
     contentImage = torch.tensor(img / 255.0).float().cuda()
     # style image
-    img = cv2.imread('./db/starry.jpg')
+    img = cv2.imread(s)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = cv2.resize(img, (1024, 768))
     styleImage = torch.tensor(img / 255.0).float().cuda()
