8bbd72b83777b7962872c40b187ad34d48efd6eb
Roth <rothdyt@gmail.com>
2021-03-21 23:11:27 -0400

    initial

---
+++ A_Neural_Algorithm_of_Artistic_Style/styleTrans.py
@@ -0,0 +1,56 @@
+'''
+File: main.py
+Author: Yutong Dai (yutongdai95@gmail.com)
+File Created: 2021-03-21 22:19
+Last Modified: 2021-03-21 23:11
+--------------------------------------------
+Description:
+'''
+import torch
+import torchvision.models as models
+import utils
+import cv2
+import torch.optim
+
+
+def style_transfer(c='./db/pikachu.jpg', s='./db/pikachu.jpg', epochs=400,
+                   c_layer=6, alpha=1, beta=6, layer_weights=0.5,
+                   optimizer='adam', lr=0.1, scheduler='none'):
+    # load model
+    model = models.vgg19(pretrained=True)
+    model = model.cuda()
+    # load image
+    # contentImage
+    img = cv2.imread()
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, (1024, 768))
+    contentImage = torch.tensor(img / 255.0).float().cuda()
+    # style image
+    img = cv2.imread('./db/starry.jpg')
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, (1024, 768))
+    styleImage = torch.tensor(img / 255.0).float().cuda()
+    layers = utils.get_layers(model)
+
+    aCs = utils.get_feature_maps(contentImage, layers, keep_grad=False)
+    aSs = utils.get_feature_maps(styleImage, layers, keep_grad=False)
+
+    if optimizer == 'adam':
+        optimizer = optim.Adam([G], lr)
+    scheduler = None
+    if scheduler != 'none':
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
+    # learn stlye + contents
+    torch.manual_seed(0)
+    G = torch.rand(contentImage.shape, requires_grad=True, device="cuda")
+    STYLE_LAYERS_WEIGHTS = [layer_weights for i in range(16)]
+    for _ in range(epochs):
+        optimizer.zero_grad()
+        imgInput = torch.transpose(G.unsqueeze(0), 1, 3)
+        aGs = utils.get_feature_maps(imgInput, layers)
+        loss = utils.compute_total_cost(aGs, aCs, aSs, STYLE_LAYERS_WEIGHTS,
+                                        content_layer_idx=c_layer, alpha=1, beta=6)
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+    return G.cpu()
---
+++ A_Neural_Algorithm_of_Artistic_Style/utils.py
@@ -0,0 +1,153 @@
+'''
+File: utils.py
+Author: Yutong Dai (yutongdai95@gmail.com)
+File Created: 2021-03-21 22:17
+Last Modified: 2021-03-21 23:10
+--------------------------------------------
+Description:
+'''
+import os
+import sys
+import scipy.io
+import scipy.misc
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+from PIL import Image
+import torch
+import numpy as np
+import pprint
+
+
+def get_layers(model):
+    layers = []
+    for idx, m in enumerate(model.features.modules()):
+        if idx > 0:
+            layers.append([m, isinstance(m, torch.nn.Conv2d)])
+    return layers
+
+
+def get_feature_maps(img, layers, keep_grad=True):
+    """
+        get outputs from conv2d layers
+    """
+    imgInput = torch.transpose(img.unsqueeze(0), 1, 3)
+    aCs = []
+    for l, isConv in layers:
+        imgInput = l(imgInput)
+        if isConv:
+            if keep_grad:
+                aCs.append(imgInput)
+            else:
+                aCs.append(imgInput.detach())
+    return aCs
+
+
+def compute_layer_content_cost(a_G, a_C):
+    """
+    Computes the content cost
+
+    Arguments:
+    a_C -- tensor of dimension (1, n_C, n_H, n_W), hidden layer activations representing content of the image C 
+    a_G -- tensor of dimension (1, n_C, n_H, n_W), hidden layer activations representing content of the image G
+
+    Returns: 
+    J_content -- scalar that you compute using equation 1 above.
+    """
+    n_bacth, n_feature_maps, n_H, n_W = a_C.shape
+    m = n_feature_maps
+    n = n_H * n_W
+    a_C_mat = a_C.view(n_bacth, n_feature_maps, -1)
+    a_G_mat = a_G.view(n_bacth, n_feature_maps, -1)
+    J_content = torch.sum((a_C_mat - a_G_mat) ** 2) / (4 * m * n)
+    return J_content
+
+
+def compute_content_cost(aGs, aCs):
+    """
+    Computes the overall style cost from several chosen layers
+
+    Arguments:
+    aGs, aCc -- activations after conv2d for G ans C images
+
+    Returns: 
+    J_content -- tensor representing a scalar value, style cost defined above
+    """
+
+    # initialize the overall style cost
+    J_content = 0.0
+    for i in range(len(aGs)):
+        layer_cost = compute_layer_content_cost(aGs[i], aCs[i])
+        J_content += layer_cost
+    return J_content
+
+
+def gram_matrix(A):
+    """
+    Argument:
+    A -- matrix of shape (n_C, n_H*n_W)
+
+    Returns:
+    GA -- Gram matrix of A, of shape (n_C, n_C)
+    """
+    GA = torch.matmul(A, A.T)
+    return GA
+
+
+def compute_layer_style_cost(a_S, a_G):
+    """
+    Arguments:
+    a_S -- tensor of dimension (1, n_C, n_H, n_W), hidden layer activations representing style of the image S 
+    a_G -- tensor of dimension (1, n_C, n_H, n_W), hidden layer activations representing style of the image G
+
+    Returns: 
+    J_style_layer -- tensor representing a scalar value, style cost defined above by equation (2)
+    """
+    n_bacth, n_feature_maps, n_H, n_W = a_S.shape
+    m = n_feature_maps
+    n = n_H * n_W
+    # dirty coding: n_bacth is expected to be 1
+    a_S_mat = a_S.view(n_bacth, n_feature_maps, -1)[0]
+    a_G_mat = a_G.view(n_bacth, n_feature_maps, -1)[0]
+    gramS = gram_matrix(a_S_mat)
+    gramG = gram_matrix(a_G_mat)
+    J_style_layer = torch.sum((gramS - gramG)**2) / ((2 * m * n)**2)
+    return J_style_layer
+
+
+def compute_style_cost(aGs, aSc, STYLE_LAYERS_WEIGHTS):
+    """
+    Computes the overall style cost from several chosen layers
+
+    Arguments:
+    aGs, aSc -- activations after conv2d for G ans S images
+    STYLE_LAYERS -- A python list containing a coefficient for each cost layer
+
+    Returns: 
+    J_style -- tensor representing a scalar value, style cost defined above
+    """
+
+    # initialize the overall style cost
+    J_style = 0.0
+    for i in range(len(STYLE_LAYERS_WEIGHTS)):
+        layer_cost = compute_layer_style_cost(aSc[i], aGs[i])
+        J_style += STYLE_LAYERS_WEIGHTS[i] * layer_cost
+    return J_style
+
+
+def compute_total_cost(aGs, aCs, aSs, style_layer_weights,
+                       content_layer_idx, alpha, beta):
+    """
+    Computes the style cost + content cost
+
+    Arguments:
+    aGs, aCs, aSs -- activations after conv2d for G, C, S images
+    STYLE_LAYERS -- A python list containing a coefficient for each cost layer
+    alpha, beta -- weights
+
+    Returns: 
+    total -- tensor representing a scalar value, cost defined above
+    """
+    content_cost = compute_content_cost(aCs[content_layer_idx], aGs[content_layer_idx])
+    style_cost = compute_style_cost(aGs, aSs, style_layer_weights)
+    total = alpha * content_cost + beta * style_cost
+    return total
