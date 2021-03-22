4501291b3863bbd54c8233dddb1a20d97c06b5a1
Roth <rothdyt@gmail.com>
2021-03-21 23:16:03 -0400

    fix bugs

--- A_Neural_Algorithm_of_Artistic_Style/styleTrans.py
+++ A_Neural_Algorithm_of_Artistic_Style/styleTrans.py
@@ -2,7 +2,7 @@
 File: main.py
 Author: Yutong Dai (yutongdai95@gmail.com)
 File Created: 2021-03-21 22:19
-Last Modified: 2021-03-21 23:11
+Last Modified: 2021-03-21 23:15
 --------------------------------------------
 Description:
 '''
@@ -15,7 +15,7 @@ import torch.optim
 
 def style_transfer(c='./db/pikachu.jpg', s='./db/pikachu.jpg', epochs=400,
                    c_layer=6, alpha=1, beta=6, layer_weights=0.5,
-                   optimizer='adam', lr=0.1, scheduler='none'):
+                   optimizer='adam', lr=0.1, scheduler='yes'):
     # load model
     model = models.vgg19(pretrained=True)
     model = model.cuda()
@@ -35,21 +35,22 @@ def style_transfer(c='./db/pikachu.jpg', s='./db/pikachu.jpg', epochs=400,
     aCs = utils.get_feature_maps(contentImage, layers, keep_grad=False)
     aSs = utils.get_feature_maps(styleImage, layers, keep_grad=False)
 
+    torch.manual_seed(0)
+    G = torch.rand(contentImage.shape, requires_grad=True, device="cuda")
+    style_layer_weights = [layer_weights for i in range(16)]
     if optimizer == 'adam':
         optimizer = optim.Adam([G], lr)
     scheduler = None
     if scheduler != 'none':
         scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
     # learn stlye + contents
-    torch.manual_seed(0)
-    G = torch.rand(contentImage.shape, requires_grad=True, device="cuda")
-    STYLE_LAYERS_WEIGHTS = [layer_weights for i in range(16)]
+
     for _ in range(epochs):
         optimizer.zero_grad()
         imgInput = torch.transpose(G.unsqueeze(0), 1, 3)
         aGs = utils.get_feature_maps(imgInput, layers)
-        loss = utils.compute_total_cost(aGs, aCs, aSs, STYLE_LAYERS_WEIGHTS,
-                                        content_layer_idx=c_layer, alpha=1, beta=6)
+        loss = utils.compute_total_cost(aGs, aCs, aSs, style_layer_weights,
+                                        content_layer_idx=c_layer, alpha=alpha, beta=beta)
         loss.backward()
         optimizer.step()
         scheduler.step()
